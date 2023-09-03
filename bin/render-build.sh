3243c1b791d14a44388325088945319730088265
CJacobe92 <jacobe.christian92@gmail.com>
2023-09-03 12:22:19 +0800

    updated build sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,10 @@
 set -o errexit
 
 # Add the environment variable here
-# export DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+export DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 
 bundle install
+bundle exec rake db:drop
+bundle exec rake db:create
 bundle exec rake db:migrate
 bundle exec rake db:seed
